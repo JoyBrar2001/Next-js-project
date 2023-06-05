feeef1765133a47eb543554ba08eb9b453ce1f08
Joy Brar <joybrar2001@gmail.com>
2023-06-05 16:40:21 +0530

    Added some more backend

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async() => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected){
+        console.log('Mongodb is already connected');
+        return;
+    }
+
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "Share Prompt",
+            useNewUrlParse: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+        console.log('MongoDB Connected');
+    } catch (error){
+        console.log(error);
+    }
+}
